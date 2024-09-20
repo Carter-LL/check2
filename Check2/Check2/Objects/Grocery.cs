616805330f4eeb2c0707f640f9919799c641a0a5
Cat <39221871+Carter-LL@users.noreply.github.com>
2024-09-20 12:04:32 -0400

    I did it!

---
+++ Check2/Check2/Objects/Grocery.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Check2.Objects
+{
+    public class Grocery : Bag
+    {
+        public string FoodColor { get; set; }
+    }
+}
---
+++ Check2/Check2/Program.cs
@@ -0,0 +1,29 @@
+﻿using Check2.Objects;
+using Newtonsoft.Json;
+using System.Text.Json.Nodes;
+
+namespace Check2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("How many records do you want to add? ");
+            var numberOfRecords = int.Parse(Console.ReadLine());
+
+            var recordList = new List<Grocery>();
+            for (int i = 0; i < numberOfRecords; i++)
+            {
+                // In this loop, populate the object's properties using Console.ReadLine()
+                var myClass = new Grocery();
+
+                Console.WriteLine("Enter the value for FoodColor");
+                myClass.FoodColor = Console.ReadLine();
+
+                recordList.Add(myClass);
+
+                Console.WriteLine(JsonConvert.SerializeObject(recordList));
+            }
+        }
+    }
+}
